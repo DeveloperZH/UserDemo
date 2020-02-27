ed84221fed83a6f7957441764ea08a93f01dab4e
DeveloperZH <345059887@qq.com>
2020-02-27 18:30:03 +0800

    新增 TextView 不同按压状态的文字的颜色

---
+++ CommonUILibrary/src/main/java/com/zh/commonuilibrary/controls/banner/XBannerView.java
@@ -0,0 +1,22 @@
+package com.zh.commonuilibrary.controls.banner;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * @describe:
+ * @author: Z H
+ * @date: 2020/2/27 16:23
+ * @pkgName: com.zh.commonuilibrary.controls.banner
+ */
+public class XBannerView extends RelativeLayout {
+    public XBannerView(Context context) {
+        super(context);
+    }
+
+    public XBannerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
---
+++ CommonUILibrary/src/main/java/com/zh/commonuilibrary/util/StateBarHelper.java
@@ -0,0 +1,13 @@
+package com.zh.commonuilibrary.util;
+
+/**
+ * @describe: app状态栏辅助类
+ * @author: Z H
+ * @date: 2020/2/27 18:01
+ * @pkgName: com.zh.commonuilibrary.util
+ */
+public class StateBarHelper {
+
+    private static final String TAG = StateBarHelper.class.getSimpleName();
+
+}
